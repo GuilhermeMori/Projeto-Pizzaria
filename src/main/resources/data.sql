b2232280f08d49d62952ab2fba1c3f0699df896c
GuilhermeMori <guilhermebmori@hotmail.com>
2022-12-28 14:03:54 +0100

    Inicio Projeto

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Clientes (id,nome, sobrenome, telefone) VALUES (1,'Guilherme', 'Mori','(41)99175-8383'); 
+INSERT INTO Clientes (id,nome, sobrenome, telefone) VALUES (2,'Ricardo', 'Gomes','(41)99552-8123'); 
+INSERT INTO Clientes (id,nome, sobrenome, telefone) VALUES (3,'Bruno', 'Felipe','(41)99175-3333'); 
+INSERT INTO Clientes (id,nome, sobrenome, telefone) VALUES (4,'Kauane', 'Borges','(41)99175-8383'); 
+INSERT INTO Clientes (id,nome, sobrenome, telefone) VALUES (5,'Thais', 'Reis','(41)99175-8383'); 
+INSERT INTO Clientes (id,nome, sobrenome, telefone) VALUES (6,'Jose', 'maria','(41)99175-8383'); 
---
+++ src/test/java/com/br/projetopizzaria/ProjetoPizzariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.projetopizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPizzariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
